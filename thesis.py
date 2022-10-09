c8743aa02befd356d0a4aff184d8d79b33b3fefd
Bobby Saba <bobbysaba@Bobbys-Mac.local>
2022-10-09 17:27:28 -0500

    calculating met variables

--- thesis.py
+++ thesis.py
@@ -5,13 +5,15 @@ import matplotlib.dates as mdates
 import numpy as np
 import pickle
 import datetime as dt
+import metpy.calc as mpcalc
+from metpy.units import units
 
 data = os.listdir('/Users/bobbysaba/Documents/Thesis/VAD')
 vad_path = '/Users/bobbysaba/Documents/Thesis/VAD'
 path = '/Users/bobbysaba/Documents/Thesis'
 
-untouched_data = ['rows', 'cols', 'height', 'no_data', 'covariance_matrix']
-
+untouched_data = ['rows', 'cols', 'height', 'no_data', 'covariance_matrix', 'lats']
+'''
 vad = {}
 
 # convert nc files to dictionary
@@ -53,6 +55,9 @@ for file in data:
         if v == 'winddir':
             vad[date]['wdir'] = vad[date]['winddir']
             del vad[date]['winddir']
+        if v == 'w':
+            vad[date][v][vad[date][v] > 10] = np.nan
+            vad[date][v][vad[date][v] < -10] = np.nan
     vad[date]['time'] = np.array(time_list)
     try:
         del vad[date]['epochtime']       
@@ -182,14 +187,14 @@ with open(path + '/vad.pickle', "wb") as output_file:
     
 with open(path + '/stare.pickle', "wb") as output_file:
     pickle.dump(stare, output_file)
-
+'''
 # open dict files 
 with open(path + '/vad.pickle', 'rb') as fh:
     vad = pickle.load(fh)
        
 with open(path + '/stare.pickle', 'rb') as fh:
     stare = pickle.load(fh)
-
+'''
 # create indexes for the start of new storm
 storm_stare = {}
 for date in stare:
@@ -282,46 +287,112 @@ for date in storm_vad:
                     if v == 'time':
                         storm_vad[date][n][v] = vad[date][v][indexes[n-1]:indexes[n]]
 
-# delete scans with no data               
-for date in storm_vad:
-    for storm in storm_vad[date]:
-        while np.nansum(storm_vad[storm][date]['wspd'][0]) == 0.0:
-            for v in storm_vad[storm][date]:
-                if v in untouched_data:
-                    continue
-                storm_vad[storm][date][v] = np.delete(storm_vad[storm][date][v], 0, 0)
-        while np.nansum(storm_vad[storm][date]['wspd'][-1]) == 0.0:
-            for v in storm_vad[storm][date]:
-                if v in untouched_data:
-                    continue
-                storm_vad[storm][date][v] = np.delete(storm_vad[storm][date][v], -1, 0)
-        
-for date in storm_stare:
-    for storm in storm_stare[date]:
-        while np.nansum(storm_stare[storm][date]['wspd'][0]) == 0.0:
-            for v in storm_stare[storm][date]:
-                if v in untouched_data:
-                    continue
-                storm_stare[storm][date][v] = np.delete(storm_stare[storm][date][v], 0, 0)
-        while np.nansum(storm_stare[storm][date]['wspd'][-1]) == 0.0:
-            for v in storm_stare[storm][date]:
-                if v in untouched_data:
-                    continue
-                storm_stare[storm][date][v] = np.delete(storm_stare[storm][date][v], -1, 0)
-
 for date in storm_stare:
     del storm_stare[date]['indexes']
     
 for date in storm_vad:
     del storm_vad[date]['indexes']
 
+# delete scans with no data               
+for date in storm_vad:
+    for storm in storm_vad[date]:
+        while np.nansum(storm_vad[date][storm]['wspd'][0]) == 0.0:
+            for v in storm_vad[date][storm]:
+                if v in untouched_data:
+                    continue
+                storm_vad[date][storm][v] = np.delete(storm_vad[date][storm][v], 0, 0)
+        while np.nansum(storm_vad[date][storm]['wspd'][-1]) == 0.0:
+            for v in storm_vad[date][storm]:
+                if v in untouched_data:
+                    continue
+                storm_vad[date][storm][v] = np.delete(storm_vad[date][storm][v], -1, 0)
+        
+for date in storm_stare:
+    for storm in storm_stare[date]:
+        while np.nansum(storm_stare[date][storm]['w'][0]) == 0.0:
+            for v in storm_stare[date][storm]:
+                if v in untouched_data:
+                    continue
+                storm_stare[date][storm][v] = np.delete(storm_stare[date][storm][v], 0, 0)
+        while np.nansum(storm_stare[date][storm]['w'][-1]) == 0.0:
+            for v in storm_stare[date][storm]:
+                if v in untouched_data:
+                    continue
+                storm_stare[date][storm][v] = np.delete(storm_stare[date][storm][v], -1, 0)
+
+# calculate bulk shear
+for date in storm_vad:
+    index_75 = np.where(vad[date]['height'] >= 0.075)[0][0]
+    index_250 = np.where(vad[date]['height'] >= 0.25)[0][0]
+    index_500 = np.where(vad[date]['height'] >= 0.5)[0][0]
+    index_1000 = np.where(vad[date]['height'] > 1)[0][0]
+    for storm in storm_vad[date]:
+        storm_vad[date][storm]['75-250_shr'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        storm_vad[date][storm]['75-500_shr'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        storm_vad[date][storm]['75-1000_shr'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        for scan in range(0, len(storm_vad[date][storm]['time'])):
+            u_comp_250 = storm_vad[date][storm]['u'][scan][index_250] - storm_vad[date][storm]['u'][scan][index_75]
+            u_comp_500 = storm_vad[date][storm]['u'][scan][index_500] - storm_vad[date][storm]['u'][scan][index_75]
+            u_comp_1000 = storm_vad[date][storm]['u'][scan][index_1000] - storm_vad[date][storm]['u'][scan][index_75]
+            v_comp_250 = storm_vad[date][storm]['v'][scan][index_250] - storm_vad[date][storm]['v'][scan][index_75]
+            v_comp_500 = storm_vad[date][storm]['v'][scan][index_500] - storm_vad[date][storm]['v'][scan][index_75]
+            v_comp_1000 = storm_vad[date][storm]['v'][scan][index_1000] - storm_vad[date][storm]['v'][scan][index_75]
+            mag_250 = ((u_comp_250 ** 2) + (v_comp_250 ** 2)) ** 0.5
+            mag_500 = ((u_comp_500 ** 2) + (v_comp_500 ** 2)) ** 0.5
+            mag_1000 = ((u_comp_1000 ** 2) + (v_comp_1000 ** 2)) ** 0.5
+            storm_vad[date][storm]['75-250_shr'][scan] = mag_250
+            storm_vad[date][storm]['75-500_shr'][scan] = mag_500
+            storm_vad[date][storm]['75-1000_shr'][scan] = mag_1000
+
+# calculate vertical shear
+for date in storm_vad:
+    for storm in storm_vad[date]:
+        storm_vad[date][storm]['vert_shear'] = np.ones(np.shape(storm_vad[date][storm]['wspd']))
+        storm_vad[date][storm]['vert_shear'][:, 0] = 0
+        for scan in range(0, len(storm_vad[date][storm]['time'])):
+            for value in range(0, len(storm_vad[date][storm]['wspd'][scan]) - 1):
+                upper = storm_vad[date][storm]['wspd'][scan][value + 1]
+                lower = storm_vad[date][storm]['wspd'][scan][value]
+                storm_vad[date][storm]['vert_shear'][scan][value + 1] = upper - lower  
+                
+# calculate storm relative helicity
+for date in storm_vad:
+    index_75 = np.where(vad[date]['height'] >= 0.075)[0][0]
+    index_250 = np.where(vad[date]['height'] >= 0.25)[0][0]
+    index_500 = np.where(vad[date]['height'] >= 0.5)[0][0]
+    index_1000 = np.where(vad[date]['height'] > 1)[0][0]
+    for storm in storm_vad[date]:
+        storm_vad[date][storm]['75-250_srh'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        storm_vad[date][storm]['75-500_srh'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        storm_vad[date][storm]['75-1000_srh'] = np.ones(np.shape(storm_vad[date][storm]['time']))
+        for scan in range(0, len(storm_vad[date][storm]['time'])):
+            height_250 = units.Quantity(storm_vad[date][storm]['height'][index_75:index_250 + 1], 'km')
+            height_500 = units.Quantity(storm_vad[date][storm]['height'][index_75:index_500 + 1], 'km')
+            height_1000 = units.Quantity(storm_vad[date][storm]['height'][index_75:index_1000 + 1], 'km')
+            u_250 = units.Quantity(storm_vad[date][storm]['u'][scan][index_75:index_250 + 1], 'm/s')
+            u_500 = units.Quantity(storm_vad[date][storm]['u'][scan][index_75:index_500 + 1], 'm/s')
+            u_1000 = units.Quantity(storm_vad[date][storm]['u'][scan][index_75:index_1000 + 1], 'm/s')
+            v_250 = units.Quantity(storm_vad[date][storm]['v'][scan][index_75:index_250 + 1], 'm/s')
+            v_500 = units.Quantity(storm_vad[date][storm]['v'][scan][index_75:index_500 + 1], 'm/s')
+            v_1000 = units.Quantity(storm_vad[date][storm]['v'][scan][index_75:index_1000 + 1], 'm/s')
+            depth_250 = height_250[-1] - height_250[0]
+            depth_500 = height_500[-1] - height_500[0]
+            depth_1000 = height_1000[-1] - height_1000[0]
+            bottom = height_250[0]
+            pos, neg, tot = mpcalc.storm_relative_helicity(height_250, u_250, v_250, depth_250, bottom = bottom)
+            storm_vad[date][storm]['75-250_srh'][scan] = tot.magnitude
+            pos, neg, tot = mpcalc.storm_relative_helicity(height_500, u_500, v_500, depth_500, bottom = bottom)
+            storm_vad[date][storm]['75-500_srh'][scan] = tot.magnitude
+            pos, neg, tot = mpcalc.storm_relative_helicity(height_1000, u_1000, v_1000, depth_1000, bottom = bottom)
+            storm_vad[date][storm]['75-1000_srh'][scan] = tot.magnitude
+        
 # save storm_data as pickle file    
 with open(path + '/storm_vad.pickle', "wb") as output_file:
     pickle.dump(storm_vad, output_file)
     
 with open(path + '/storm_stare.pickle', "wb") as output_file:
     pickle.dump(storm_stare, output_file)
-
+'''
 # open storm_data files 
 with open(path + '/storm_vad.pickle', 'rb') as fh:
     storm_vad = pickle.load(fh)
@@ -329,25 +400,53 @@ with open(path + '/storm_vad.pickle', 'rb') as fh:
 with open(path + '/storm_stare.pickle', 'rb') as fh:
     storm_stare = pickle.load(fh)
 
+
 for date in storm_vad:
-    if date[0:4] == '2022':
-        for storm in storm_vad[date]:
-            if type(storm) == int:
-                figure, (ax1,ax2) = plt.subplots(2, 1, sharex = True)
-                x = storm_vad[date][storm]['time']
-                y = storm_vad[date][storm]['height']
-                z = np.transpose(storm_vad[date][storm]['wspd'])
-                z_2 = np.transpose(storm_vad[date][storm]['wdir'])
-                speed = ax1.contourf(x, y, z)
-                dir = ax2.contourf(x, y, z_2)
-                plt.xlabel('time (UTC)')
-                myFmt = mdates.DateFormatter('%H:%M')
-                plt.gca().xaxis.set_major_formatter(myFmt)
-                figure.text(0.04, 0.5, 'height above lidar (km)', va = 'center', rotation = 'vertical')
-                plt.suptitle('Storm ' + str(storm) + ' Wind Speed/Direction on ' + date)
-                plt.colorbar(speed, ax=ax1, label = 'wind speed')
-                plt.colorbar(dir, ax=ax2, label = 'wind direction')
-    
+    for storm in storm_vad[date]:
+        if type(storm) == int:
+            figure, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, constrained_layout = True, sharex = True)
+            # set plotting variables 
+            x = storm_vad[date][storm]['time']
+            y = storm_vad[date][storm]['height']
+            z = np.transpose(storm_vad[date][storm]['wspd'])
+            z_2 = np.transpose(storm_vad[date][storm]['wdir'])
+            z_3 = np.transpose(storm_vad[date][storm]['w'])
+            z_4 = storm_vad[date][storm]['75-250_shr']
+            z_5 = storm_vad[date][storm]['75-500_shr']
+            z_6 = storm_vad[date][storm]['75-1000_shr']
+            # wind speed
+            speed = ax1.contourf(x, y, z, cmap = 'rainbow')
+            ax1.text(x[0], 2.45, 'horiz. wind speed (m/s)')
+            plt.colorbar(speed, ax = ax1)
+            ax1.set(ylabel = 'heihgt (km)')
+            # wind dir
+            dir = ax2.contourf(x, y, z_2)
+            plt.colorbar(dir, ax = ax2)
+            ax2.text(x[0], 2.45, 'horiz. wind dir. (˚)')
+            ax2.set(ylabel = 'heihgt (km)')
+            # vertical wind speed
+            vert = ax3.contourf(x,y,z_3, cmap = 'seismic')
+            plt.colorbar(vert, ax = ax3, ticks = [-10, -5, 0, 5, 10])
+            ax3.text(x[0], 2.45, 'vert. wind speed (m/s)')
+            ax3.set(ylabel = 'heihgt (km)')
+            # bulk shear
+            ax4.plot(x, z_4, color = 'green', label = '75-250m')
+            ax4.plot(x, z_5, color = 'blue', label = '75-500m')
+            ax4.plot(x, z_6, color = 'red', label = '75-1000m')
+            ax4.set(ylabel = 'shear (m/s)')
+            ax4.legend(loc = 'upper left', ncol = 3, prop = {'size':8})
+            # plot shared x label
+            plt.xlabel('time (UTC)')
+            myFmt = mdates.DateFormatter('%H:%M')
+            plt.gca().xaxis.set_major_formatter(myFmt)
+            # general formatting
+            plt.suptitle('Storm ' + str(storm) + ' Wind Speed/Direction on ' + date)
+     
+            
+            
+            
+            
+
 
 
 
